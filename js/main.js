82f53f4554f1530c845a8472f47ec0b9171a0a13
dnorkett <dnorkett@gmail.com>
2020-04-18 20:14:06 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+tabItems.forEach(item => item.addEventListener('click', selectItem));
+
+
+selectItem = function(e) {
+    removeBorder();
+    removeShow();
+
+    this.classList.add('tab-border');
+
+    const tabContentItem = document.querySelector(`#${this.id}-content`)
+    tabContentItem.classList.add('show');
+
+}
+
+removeBorder = function() {
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+
+removeShow = function() {
+    tabContentItems.forEach(item => item.classList.remove('show'))
+}
